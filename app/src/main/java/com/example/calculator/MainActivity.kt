2dad0414c2e350e4c06e387f23cfff287f749268
t179a <azmtzk179@gmail.com>
2022-12-11 03:16:41 +0900

    UIとロジックを連携

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    CalculatorScreen(modifier = Modifier.fillMaxSize())
+                    CalculatorScreen(viewModel = calculatorViewModel, modifier = Modifier.fillMaxSize())
                 }
             }
         }
